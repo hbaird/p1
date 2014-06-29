eb72a6efda3a6e3e5379c14d8823e14d5d8d3d38
Heather Baird <hbaird@fas.harvard.edu>
2014-06-29 19:17:46 -0400

    Added p1.php

---
+++ p1.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Heather Baird P1</title>
+	<meta charset="utf-8">
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel='stylesheet' href='//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css' type='text/css'>
+	<link href="//netdna.bootstrapcdn.com/bootswatch/3.1.1/flatly/bootstrap.min.css" rel="stylesheet">
+
+	<style>
+	
+		h1 {
+			text-align:center;
+		}
+	
+		.container {
+			width:50%;
+			margin-bottom:50px;
+		}	
+		
+		img {
+			display:block;
+			margin:auto;
+			width:100%;
+		}
+	
+	</style>
+
+</head>
+
+<body>
+	<div class"container">
+		<h1>Heather Baird</h1>
+
+		<img src="images/withalligator.jpg alt="me with alligator">
+
+		<h2>In 2+ sentences describe who you are and what you do.</h2>
+		<p></p>
+
+		<h2>What previous experience do you have with HTML/CSS?</h2>
+		<p></p>
+
+		<h2>What previous experience do you have with programming?</h2>
+		<p></p>
+
+		<h2>Have you worked with PHP before? If so, to what extent?</h2>
+		<p></p>
+
+		<h2>Are you working on a Mac, PC, or some other platform?</h2>
+		<p></p>
+
+		<h2>Anything else we should know?</h2>
+		<p></p>
+		
+		<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
+	</div>
+
+</body>
+</html>
