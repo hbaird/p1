1a595a363c1d0dbe68b2706c69e4b687dac7ee4a
Heather Baird <hbaird@fas.harvard.edu>
2014-06-29 20:37:54 -0400

    third commit

--- p1.php
+++ p1.php
@@ -32,28 +32,36 @@
 </head>
 
 <body>
-	<div class"container">
+	<div class="container">
 		<h1>Heather Baird</h1>
 
-		<img src="images/withalligator.jpg alt="me with alligator">
+		<img src="/images/withalligator.jpg alt="me with alligator">
 
 		<h2>In 2+ sentences describe who you are and what you do.</h2>
-		<p></p>
+		<p>Hello! I am currently a full-time student pursuing an undergraduate degree in 
+		Applied Mathematics at Harvard Extension School. This is my second summer taking 
+		classes during summer. I spend the majority of my time studying, but when I can, 
+		I try to take pictures of the night sky, building furniture, read ancient Latin texts,
+		and code.</p>
 
 		<h2>What previous experience do you have with HTML/CSS?</h2>
-		<p></p>
+		<p>I have taken CSCI E-12, Fundamentals of Website Development, 
+		and Jen Kramer’s class on CMS technologies. My experience with HTML/CSS, or any coding
+		language, has been purely academic</p>
 
 		<h2>What previous experience do you have with programming?</h2>
-		<p></p>
+		<p>I completed the Introduction to Computer Science course CS50. I have some limited
+		experience with programming in C and Java</p>
 
 		<h2>Have you worked with PHP before? If so, to what extent?</h2>
-		<p></p>
+		<p>I have used PHP to code a project in the past. I am looking forward to learning more 
+	    more about PHP this summer.</p>
 
 		<h2>Are you working on a Mac, PC, or some other platform?</h2>
-		<p></p>
+		<p>I am a using a PC. Sorry.</p>
 
 		<h2>Anything else we should know?</h2>
-		<p></p>
+		<p>I love crocodiles and alligators!</p>
 		
 		<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 
