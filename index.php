08d7f0c55045526c7cae0c44709a7d4e2b6ed90b
Heather Baird <hbaird@fas.harvard.edu>
2014-06-30 17:22:31 -0400

    Changed p1.php to index.php

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Heather Baird P1</title>
+	<meta charset="utf-8">
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel='stylesheet' href='//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css' type='text/css'>
+	<link href="//netdna.bootstrapcdn.com/bootswatch/3.1.1/flatly/bootstrap.min.css" rel="stylesheet">
+
+	<style>
+	
+		h1, h5 {
+			text-align:center;
+			text-font: ;
+		}
+	
+		.container {
+			width:50%;
+			margin-bottom:50px;
+		}	
+		
+		img {
+			display:block;
+			margin:auto;
+			width:100%;
+		}
+	
+	</style>
+
+</head>
+
+<body>
+	<div class="container">
+		<h1>Heather Baird</h1>
+
+		<img src='images/withalligator.jpg' alt='me with alligator'>
+		<h5>That's me with an alligator.</h5>
+
+		<h2>In 2+ sentences describe who you are and what you do.</h2>
+		<p>Hello! I am currently a full-time student pursuing an undergraduate degree in 
+		Applied Mathematics at Harvard Extension School. I spend the majority of my time studying,
+		but when I can, I try to take pictures of the night sky, build furniture, read ancient Latin texts,
+		and code.</p>
+
+		<h2>What previous experience do you have with HTML/CSS?</h2>
+		<p>I have taken CSCI E-12, Fundamentals of Website Development, 
+		and Jen Kramer’s class on CMS technologies. My experience with HTML/CSS, or any coding
+		language, has been purely academic</p>
+
+		<h2>What previous experience do you have with programming?</h2>
+		<p>I completed the Introduction to Computer Science course, CS50. I have some limited
+		experience with programming in C and Java</p>
+
+		<h2>Have you worked with PHP before? If so, to what extent?</h2>
+		<p>I have used PHP to code a project in the past. I am looking forward to learning
+	    more about PHP this summer.</p>
+
+		<h2>Are you working on a Mac, PC, or some other platform?</h2>
+		<p>I am a using a PC.</p>
+
+		<h2>Anything else we should know?</h2>
+		<p>I love crocodiles and alligators!</p>
+		
+		<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
+	</div>
+
+</body>
+</html>
