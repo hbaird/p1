57c91540d576b5bdd58011cd8892d24315a5ee11
Heather Baird <hbaird@fas.harvard.edu>
2014-06-30 18:34:07 -0400

    Full Project Update

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-This is the readme file for Project 1.
+<h1>Heather Baird Project 1.</h1>
 
-The live URL for Project 1 is   http://p1-heather.gopagoda.com.
+<h1>Live URL</h1>
+<http://p1-heather.gopagoda.com>
 
+<h1>Description</h1>
+A simple webpage that utilizes the concepts taught in lectures 1 and 2.
+
+<h1>Details for Teaching Team</h1>
 There are no test credentials.
 
-No external code sources were used.
+<h1>Outside Code</h1>
+[Bootstrap]: http://getbootstrap.com/ 
 
--- index.php
+++ index.php
@@ -2,33 +2,21 @@
 <html>
 <head>
 
-	<title>Heather Baird P1</title>
+	<title>Heather Baird Project 1</title>
 	<meta charset="utf-8">
 	
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<link rel='stylesheet' href='//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css' type='text/css'>
-	<link href="//netdna.bootstrapcdn.com/bootswatch/3.1.1/flatly/bootstrap.min.css" rel="stylesheet">
+	<!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
 
-	<style>
-	
-		h1, h5 {
-			text-align:center;
-			text-font: ;
-		}
-	
-		.container {
-			width:50%;
-			margin-bottom:50px;
-		}	
-		
-		img {
-			display:block;
-			margin:auto;
-			width:100%;
-		}
-	
-	</style>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 
 </head>
 
